2471eca328877360e83a2fec08ea0210a59058aa
jpbetanza <jpbetanza99@gmail.com>
2023-12-19 19:59:49 -0300

    expecting changes here

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useEffect, useState } from 'react'
 import MainFrame from './components/MainFrame'
 import Video from './components/Video'
+import Init from './components/Init'
 
 function App() {
   const [init, setInit] = useState(false)
 
   return (
-
     <div>
-      <MainFrame/>
       <Video/>
+      <MainFrame/>
+      {/* {init?(<Init/>):(<MainFrame/>)} */}
     </div>
   )
 }
